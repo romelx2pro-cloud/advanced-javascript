ddf103d057eb8f2e9013cfd7d2360d7f1cb50e15
Md. Rafiqul Islam Romel <romelx2pro@gmail.com>
2026-05-17 21:20:00 +0600

    null vs undefined  values

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+// let pakhi;
+// console.log(pakhi);
+// function a(n1, n2) {
+// console.log(n1 + n2);
+// }
+// function a(n1, n2) {
+// console.log(n1, n2);
+// }
+// const result = a(13);
+// const premik = { name: "smart dude", phone: 123456 };
+// console.log(premik.name);
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
