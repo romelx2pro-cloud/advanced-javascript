9e9fc1c698498922e5ee6ed437c77fad8e74cd7c
Md. Rafiqul Islam Romel <romelx2pro@gmail.com>
2026-05-17 21:01:30 +0600

    Truthy vs Falsy values

---
+++ trueFalse.js
@@ -0,0 +1,16 @@
+//Falsy
+// 0
+// ""
+// false
+// undefined
+// null
+// NaN
+// Truthy
+// '0', " ",[],{}
+const age = 0;
+const name = "";
+if (name) {
+  console.log("condition is true");
+} else {
+  console.log("condition is false");
+}
